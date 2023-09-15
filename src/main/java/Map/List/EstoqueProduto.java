2766c8b78884a4b901007d40a1ca79ab560d7be2
ALESSANDRODASILVALIMA <alessandrodasilva93@gmail.com>
2023-09-14 22:20:26 -0300

    Operações basica com map concluido

--- src/main/java/Map/List/EstoqueProduto.java
+++ src/main/java/Map/List/EstoqueProduto.java
@@ -22,11 +22,38 @@ public class EstoqueProduto {
         System.out.println("Total: " + total);
     }
 
+    public void obterProdutoMaiorValorDeAcordoComQuantidade(){
+        double total = 0;
+        double maiorTotalNaQtd = 0;
+        int chave = 0;
+        for (int key : estoqueProduto.keySet()){
+            total = (estoqueProduto.get(key).getPreco() * estoqueProduto.get(key).getQtd());
+            if(total > maiorTotalNaQtd){
+                maiorTotalNaQtd = total;
+
+                chave = key;
+            }
+        }
+        System.out.println("Maior Total: " + estoqueProduto.get(chave));
+    }
+
     public void obterProdutoMaisCaro(){
         double maiorValor = Double.MIN_VALUE;
         int keyMenorPreco = 0;
         for (int key : estoqueProduto.keySet()){
             if(estoqueProduto.get(key).getPreco() > maiorValor){
+                maiorValor = estoqueProduto.get(key).getPreco();
+                keyMenorPreco = key;
+            }
+        }
+        System.out.println(estoqueProduto.get(keyMenorPreco));
+    }
+
+    public void obterProdutoMenosCaro(){
+        double maiorValor = Double.MAX_VALUE;
+        int keyMenorPreco = 0;
+        for (int key : estoqueProduto.keySet()){
+            if(estoqueProduto.get(key).getPreco() < maiorValor){
                 maiorValor = estoqueProduto.get(key).getPreco();
                 keyMenorPreco = key;
             }
@@ -40,12 +67,14 @@ public class EstoqueProduto {
 
     public static void main(String[] args) {
         EstoqueProduto ep = new EstoqueProduto();
-        ep.adicionarProduto(1, "Café", 8.0, 2);
+        ep.adicionarProduto(1, "Café", 8.50, 2);
         ep.adicionarProduto(2, "Oleo", 9.0, 2);
-        ep.adicionarProduto(4, "Café", 8.0, 2);
+        ep.adicionarProduto(4, "Café Soluvel", 7.0, 2);
         ep.exibirProduto();
         ep.calcularValorTotalEstoque();
 
         ep.obterProdutoMaisCaro();
+        ep.obterProdutoMenosCaro();
+        ep.obterProdutoMaiorValorDeAcordoComQuantidade();
     }
 }
